d1add4a4e2103c7cd766fef6a57d2b3846e6030f
WrJamshid <wr.jamshid@gmail.com>
2022-12-01 12:28:25 +0500

    header and hover and modal finished

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+
+function toggle(){
+  var blur = document.getElementById('blur');
+  blur.classList.toggle('active');
+  var modal = document.getElementById('modal');
+  modal.classList.toggle('active');
+}
